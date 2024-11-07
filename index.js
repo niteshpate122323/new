f9bb1e0719d8568b689092ec5e67e6c6adf5c43a
niteshpate122323 <niteshpatel.biz@gmail.com>
2024-11-07 16:42:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+  document.addEventListener( 'DOMContentLoaded', function () {
+            new Splide( '#image-carousel', {
+                cover      : true,
+                rewind     : true,
+                pagination : false,
+                rewindSpeed: 1000,
+                rewindByDrag: true,
+                heightRatio: 0.3,
+            } ).mount();
+        } );
 
